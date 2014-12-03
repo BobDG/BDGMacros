9b4f85f818c136a827577b5a11c50de3ba54c1b3
Bob de Graaf <graafict@gmail.com>
2014-12-03 21:32:52 +0100

    Added uicolor from hex-code macro!

--- BDGMacros.h
+++ BDGMacros.h
@@ -63,6 +63,13 @@ NSLog(@"-- Font: %@", fontStrings); \
 } \
 }
 
+//UIColor from HEX (if you have a #, simply replace it by 0x)
+#define UIColorFromRGB(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+alpha:1.0]
+
 //LogSimulator Docfolder
 #define logSimDoc NSLog(@"Documents Directory: %@", [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
 
